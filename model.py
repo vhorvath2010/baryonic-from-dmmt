43635443bac1dede956ca8c8bde067168ad18c22
Vince <vhorvath2010@gmail.com>
2024-01-22 11:08:34 -0500

    quick and dirty prediction plotting

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 from torch_geometric.nn import GCNConv
 
+
+# Model arch
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -10,7 +12,7 @@ class GCN(torch.nn.Module):
         self.conv3 = GCNConv(128, 128)
         self.conv4 = GCNConv(128, 128)
         self.conv5 = GCNConv(128, 128)
-        
+
         self.lin = torch.nn.Linear(128, 1)
         self.leaky = torch.nn.LeakyReLU(negative_slope=0.01)
 
@@ -19,21 +21,21 @@ class GCN(torch.nn.Module):
 
         x = self.conv1(x, edge_index)
         x = self.relu(x)
-        
+
         x = self.conv2(x, edge_index)
         x = self.relu(x)
-        
+
         x = self.conv3(x, edge_index)
         x = self.relu(x)
-        
+
         x = self.conv4(x, edge_index)
         x = self.relu(x)
-        
+
         x = self.conv5(x, edge_index)
         x = self.relu(x)
-        
+
         x = self.lin(x)
-        
+
         x = self.leaky(x)
 
         return x
