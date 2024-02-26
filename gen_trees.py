7e539fcbd6e761680e5bf67b2ade5da4620f15fc
Vince <vhorvath2010@gmail.com>
2024-02-25 20:23:29 -0500

    fix ancestor mapping

--- gen_trees.py
+++ gen_trees.py
@@ -13,6 +13,7 @@ graphs = []
 print("loading and pruning all trees")
 for tree in trees:
     ids_in_graph = {}  # hold uid -> idx pairs
+    uids_in_x = set()
     x = []
     edge_index = []  # add edges as [parent, child] and transpose afterward
 
@@ -21,10 +22,13 @@ for tree in trees:
         curr = leaf
         while curr.descendent is not None:  # run to root (which has no descendent)
             curr = curr.descendent
-            # Ensure node isn't already in graph
+            # Check if mapping exist
             if curr.uid not in ids_in_graph:
                 # Save id mapping
                 ids_in_graph[curr.uid] = len(ids_in_graph)
+
+            # Ensure node isn't already in graph (should never not be the case...)
+            if curr.uid not in uids_in_x:
                 # Save x values
                 pos = curr["position"].value.tolist()
                 rVir = curr["virial_radius"].value
@@ -37,9 +41,14 @@ for tree in trees:
                     rVir.item(),
                 ]
                 x.append(node_data)
+                uids_in_x.add(curr.uid)
 
             # Generate edge from ancestor
             for ancestor in curr.ancestors:
+                # Check if mapping exist
+                if ancestor.uid not in ids_in_graph:
+                    # Save id mapping
+                    ids_in_graph[ancestor.uid] = len(ids_in_graph)
                 edge = [
                     ids_in_graph[ancestor.uid],
                     ids_in_graph[curr.uid],
