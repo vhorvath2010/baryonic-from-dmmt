69a6bc49390906bac1787290cb18203b3ec01960
Vince <vhorvath2010@gmail.com>
2024-02-25 20:30:03 -0500

    change how we iterate

--- gen_trees.py
+++ gen_trees.py
@@ -18,52 +18,51 @@ for tree in trees:
     edge_index = []  # add edges as [parent, child] and transpose afterward
 
     # start from leaf nodes (no ancestors)
-    for leaf in tree.get_leaf_nodes():
-        curr = leaf
-        while curr.descendent is not None:  # run to root (which has no descendent)
-            curr = curr.descendent
+    for curr in tree["tree"]:
+        if curr.uid not in ids_in_graph:
+            # Save id mapping
+            ids_in_graph[curr.uid] = len(ids_in_graph)
+
+        # Ensure node isn't already in graph (should never not be the case...)
+        if curr.uid not in uids_in_x:
+            # Save x values
+            pos = curr["position"].value.tolist()
+            rVir = curr["virial_radius"].value
+            node_data = [
+                curr["mass"].value.item(),
+                curr["redshift"],
+                pos[0],
+                pos[1],
+                pos[2],
+                rVir.item(),
+            ]
+            x.append(node_data)
+            uids_in_x.add(curr.uid)
+
+        # Generate edge from ancestor
+        for ancestor in curr.ancestors:
             # Check if mapping exist
-            if curr.uid not in ids_in_graph:
+            if ancestor.uid not in ids_in_graph:
                 # Save id mapping
-                ids_in_graph[curr.uid] = len(ids_in_graph)
-
-            # Ensure node isn't already in graph (should never not be the case...)
-            if curr.uid not in uids_in_x:
-                # Save x values
-                pos = curr["position"].value.tolist()
-                rVir = curr["virial_radius"].value
-                node_data = [
-                    curr["mass"].value.item(),
-                    curr["redshift"],
-                    pos[0],
-                    pos[1],
-                    pos[2],
-                    rVir.item(),
-                ]
-                x.append(node_data)
-                uids_in_x.add(curr.uid)
-
-            # Generate edge from ancestor
-            for ancestor in curr.ancestors:
-                # Check if mapping exist
-                if ancestor.uid not in ids_in_graph:
-                    # Save id mapping
-                    ids_in_graph[ancestor.uid] = len(ids_in_graph)
-                edge = [
-                    ids_in_graph[ancestor.uid],
-                    ids_in_graph[curr.uid],
-                ]  # add edge from last relevant to curr (skipping non-relevant nodes in-between)
-                if edge not in edge_index:
-                    edge_index.append(edge)
+                ids_in_graph[ancestor.uid] = len(ids_in_graph)
+            edge = [
+                ids_in_graph[ancestor.uid],
+                ids_in_graph[curr.uid],
+            ]  # add edge from last relevant to curr (skipping non-relevant nodes in-between)
+            if edge not in edge_index:
+                edge_index.append(edge)
 
     x = torch.tensor(x, dtype=torch.float32)
     edge_index = torch.tensor(edge_index).T
     graph = torch_geometric.data.Data(x=x, edge_index=edge_index)
-    if not graph.validate():
-        print("Invalid graph found!")
-    else:
+    try:
+        graph.validate()
         graphs.append(graph)
         print(f"Graph #{len(graphs)} was saved!")
+    except Exception as error:
+        print("Invalid graph found!")
+        print(error)
+
 
 print(f"Saving {len(graphs)} graphs...")
 torch.save(graphs, "datasets/unpruned/SG256.pt")  # Set save location here
